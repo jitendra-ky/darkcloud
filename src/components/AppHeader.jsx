8ff0b8e6e4c1c8e7336d95c38482d6be92bac398
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-07-30 12:00:35 +0530

    wireframe design almost ready

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,15 @@
+// this is the AppMain component
+
+import React from 'react';
+
+const AppMain = () => {
+    // Your component logic here
+
+    return (
+        <div className='nav-bar'>
+            <p>this is AppMain component</p>
+        </div>
+    );
+};
+
+export default AppMain;
---
+++ src/components/AppMain.jsx
@@ -0,0 +1,29 @@
+// this the AppHeader component
+
+const AppHeader = () => {
+    return (
+        <div className="main-div">
+            <p>This is the App-header</p>
+            <SelectArea />
+            <ViewInfo />
+        </div>
+    );
+};
+
+const SelectArea = () => {
+    return (
+        <div className="select-area">
+            <p>This is the SelectArea component</p>
+        </div>
+    );
+}
+
+const ViewInfo = () => {
+    return (
+        <div className="view-info">
+            <p>This is the ViewInfo component</p>
+        </div>
+    );
+}
+
+export default AppHeader;
