f8a4ec06e691e39c34fb9addb190328682e23616
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-08-01 12:57:57 +0530

    header responsive

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -12,7 +12,7 @@ const AppHeader = () => {
                 <h1>Dark-Cloud</h1>
                 
             </div>
-            <p> Created by <a href="https://www.linkedin.com/in/jitendra~kumar/">@Jitendra~Kumar</a> & <a href="https://www.linkedin.com/in/ayush-rajput-199574287/">@Ayush-Rajput</a></p>
+            <p> Created by <a className='jk' href="https://www.linkedin.com/in/jitendra~kumar/">@Jitendra~Kumar</a> & <a className='ar' href="https://www.linkedin.com/in/ayush-rajput-199574287/">@Ayush-Rajput</a></p>
         </div>
     );
 };
